41b766aa1c099da6efa69968b376afb24446e9a1
QZL <HomeLaptop>
2025-04-13 01:31:49 +0800

    achieve boss attack varient 2

--- Assets/Scripts/Character/Enemy/Fake_Knight/SubObjects/ShockWave.cs
+++ Assets/Scripts/Character/Enemy/Fake_Knight/SubObjects/ShockWave.cs
@@ -9,29 +9,27 @@ public class ShockWave : MonoBehaviour
 
     [Header("Object info")]
     [SerializeField] private float maxTravelDistance = 10f;
-    [SerializeField] private LayerMask whatisGround;
     [SerializeField] private float xVelocity;
     [SerializeField] private int damage;
-
-    private Fake_Knight boss;
-    private Transform objectTransform;
+    
     public Rigidbody2D rb { get; private set; }
     public PolygonCollider2D polygonCollider { get; private set; }
     private Vector2 startPos;
+    private int direction = 1;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         polygonCollider = GetComponent<PolygonCollider2D>();
-        boss = GetComponentInParent<Fake_Knight>();
-        objectTransform = boss.attackCheck.transform;
+    }
 
+    public void SetDirection(int dir)
+    {
+        direction = dir;
     }
 
     private void OnEnable()
     {
-        Debug.Log(boss);
-        InitPosition();
         startPos = transform.position;
     }
 
@@ -39,28 +37,11 @@ public class ShockWave : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        rb.velocity = new Vector2(xVelocity * boss.facingDirection, 0);
+        rb.velocity = new Vector2(xVelocity * direction, 0);
 
-        if (Vector3.Distance(transform.position, startPos) >= maxTravelDistance)
+        if (Vector2.Distance(transform.position, startPos) >= maxTravelDistance)
         {
             ObjectManager.instance.ReturnObjectItem(objectTag, gameObject);
-        }
-    }
-
-    private void InitPosition()
-    {
-        // 从 objectTransform 向下发射一条射线，检测地面
-        RaycastHit2D hit = Physics2D.Raycast(objectTransform.position, Vector2.down, 10f, whatisGround);
-
-        if (hit.collider != null)
-        {
-            // 设置波的位置：X 使用攻击点，Y 使用地面高度
-            transform.position = new Vector2(objectTransform.position.x, hit.point.y);
-        }
-        else
-        {
-            // 如果没检测到地面，保持原位置
-            transform.position = objectTransform.position;
         }
     }
 
--- Assets/Scripts/Character/Enemy/Fake_Knight/Tasks/SpawnShockWave.cs
+++ Assets/Scripts/Character/Enemy/Fake_Knight/Tasks/SpawnShockWave.cs
@@ -5,6 +5,8 @@ using BehaviorDesigner.Runtime.Tasks;
 public class SpawnShockWave : Fake_Knight_Action
 {
     public string objectTag = "ShockWave";
+    public LayerMask whatisGround;
+    private GameObject shockWave;
     // Start is called before the first frame update
     public override void OnStart()
     {
@@ -13,8 +15,26 @@ public class SpawnShockWave : Fake_Knight_Action
 
     private void Generate()
     {
-        ObjectManager.instance.GetObjectItem(objectTag);
+        shockWave = ObjectManager.instance.GetObjectItem(objectTag);
+
+        RaycastHit2D hit = Physics2D.Raycast(boss.attackCheck.position, Vector2.down, 10f, whatisGround);
+
+        if (hit.collider != null)
+        {
+            // 设置波的位置：X 使用攻击点，Y 使用地面高度
+            shockWave.transform.position = new Vector2(boss.attackCheck.position.x, hit.point.y);
+        }
+        else
+        {
+            // 如果没检测到地面，保持原位置
+            shockWave.transform.position = boss.attackCheck.position;
+        }
+
+        //通过方法注入参数
+        var wave = shockWave.GetComponent<ShockWave>();
+        wave.SetDirection(boss.facingDirection);
     }
+
     public override TaskStatus OnUpdate()
     {
         return TaskStatus.Success;
